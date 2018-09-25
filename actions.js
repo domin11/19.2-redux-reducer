7c9e714c16790d612e8a1db63a70e6f79236eee4
Dominik Soboń <dominik.sobon@gmail.com>
2018-09-25 14:37:19 +0200

    Add basic files

---
+++ actions.js
@@ -0,0 +1,61 @@
+import uuid from uuid;
+
+const ADD_COMMENT = 'ADD_COMMENT';
+const REMOVE_COMMENT = 'REMOVE_COMMENT';
+const EDIT_COMMENT = 'EDIT_COMMENT';
+const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+// Dodawanie komentarza
+function addComment(text) {
+  return {
+    type: ADD_COMMENT,
+    text,
+    id: uuid.v4()
+  }
+}
+
+const boundAddComment = text => dispatch(addComment(text));
+
+// Usuwanie komentarza
+function removeComment(id) {
+  return {
+    type: REMOVE_COMMENT,
+    id: id
+  }
+}
+
+const boundRemoveComment = id => dispatch(removeComment(id));
+
+// Edytowanie komentarza
+function editComment(text, id) {
+  return {
+    type: EDIT_COMMENT,
+    text: 'Nowy tekst',
+    id: id
+  }
+}
+
+const boundEditComment = text, id => dispatch(editComment(text, id));
+
+// Łapka w górę
+function thumbUpComment(id, like) {
+  return {
+    type: THUMB_UP_COMMENT,
+    id: id,
+    like: ++like
+  }
+}
+
+const boundThumbUpComment = id, like => dispatch(thumbUpComment(id, like));
+
+// Łapka w dół
+function thumbDowmComment(id, dislike) {
+  return {
+    type: THUMB_DOWN_COMMENT,
+    id: id,
+    dislike: ++dislike
+  }
+}
+
+const boundThumbDownComment = id, dislike => dispatch(thumbDownComment(id, dislike));
